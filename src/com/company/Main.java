864af222ded3c180eda164435b2d8e2f5198e2bf
Volodymyr_Moisei <volodymyr.moisei@gmail.com>
2020-09-07 11:12:11 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String sentence = "gAWs";
+        String palindrome="";
+        for (int i = 0; i < sentence.length(); i++)
+        {
+            if(Character.isLetter(sentence.charAt(i)))
+            {
+                palindrome +=sentence.charAt(i);
+            }
+        }
+        palindrome = palindrome.toLowerCase();
+        boolean isPalindrome = true;
+        for (int i = 0; i<palindrome.length()/2; i++)
+        {
+
+            if (palindrome.charAt(i) != palindrome.charAt(palindrome.length()-i-1))
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome)
+        {
+            System.out.println(sentence + " - паліндром.");
+        }
+        else
+        {
+            System.out.println(sentence + " - не паліндром");
+        }
+    }
+}
